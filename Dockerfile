7461f28c7dac881ef069dfff24b6d86c3ce5183a
rafi <shultan.faris157@gmail.com>
2025-09-17 15:50:37 +0700

    Cleaning file directories for final production stage.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.11-slim
-# Create new dir on the container
+
 WORKDIR /app
 
 RUN apt-get update && apt-get install -y netcat-openbsd && \
@@ -9,21 +9,11 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY /src ./src/
-COPY wait-broker.sh /usr/local/bin/wait-broker.sh
 RUN chmod +x /usr/local/bin/wait-broker.sh
 
-# Best practice for environment variables (don't copy .env)
-# ENV vars should be passed via docker-compose or runtime
-
-# Health check (adjust as needed)
 HEALTHCHECK --interval=30s --timeout=30s --start-period=5s --retries=5 \
     CMD nc -z kafka 9092 || exit 1
-    #CMD python -c "from kafka import KafkaConsumer; KafkaConsumer(bootstrap_servers=['kafka:9092'])"
-    #CMD python -c "from confluent_kafka import Consumer; Consumer({'bootstrap.servers':'kafka:9092})"
 
-# Use ENTRYPOINT for the wait script and CMD for the main process
-#ENTRYPOINT ["wait-consumer.sh"]
-#CMD ["python", "src/s3_client.py", "src/consumer.py", "src/producer.py"] 
 
 
 
--- src/consumer.py
+++ src/consumer.py
@@ -1,126 +1,202 @@
-import numpy as np
-import pandas as pd
-from kafka import KafkaConsumer, KafkaProducer
-from kafka.errors import KafkaError
-from time import sleep
 import json
-import time
 import logging
-from datetime import datetime, timezone
-import requests
-from dotenv import load_dotenv
-import os
-from pathlib import Path
-from s3_client import S3Uploader
+import signal
+import sys
+import time
+import threading
 import io
-import pyarrow
+import os
+import secrets
+from dotenv import load_dotenv
+from datetime import datetime, timezone, timedelta
+from confluent_kafka import Consumer, KafkaError, KafkaException
+import boto3
+from botocore.exceptions import BotoCoreError, ClientError
+from collections import defaultdict
 
 load_dotenv('/app/.env')
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    handlers=[logging.StreamHandler()]
-)
-logger = logging.getLogger('kafka-consumer-1')
-logging.info("Starting kafka consumer 1 . . .")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger("consumer-1")
 
-class resilientConsumer:
-    def __init__(self, host, port, topic_name, bucket_name):
-        # Kafka server
-        self.topic_name=topic_name
-        self.host=host
-        self.port=port
-        # Batching
-        self.batch = []
-        self.batch_size = 15
-        # Error handling
-        self.max_retries = 3
-        self.max_delays = 5
-        # S3 upload func
-        self.bucket_name = bucket_name
-        self.s3_uploader = S3Uploader(bucket_name=bucket_name) # S3 uploader
-        # Create consumer object
-        self.consumer = self._create_consumer()
+# Consumer Config
+CONSUMER_CONF = {
+    'bootstrap.servers':f"{os.environ['HOST']}:{os.environ['PORT']}",
+    'group.id':os.environ['GROUP_ID'],
+    'auto.offset.reset':'earliest'
+}
+TOPIC = os.environ['TOPIC']
 
-    def __iter__(self):
-        return self.consumer.__iter__()
+# S3 Bucket Config
+AWS_REGION = os.environ['AWS_REGION']
+BUCKET = os.environ['BUCKET']
+S3_PREFIX = ""
+
+# Batching Config
+WINDOW_MINUTES = 15 # In minutes
+FLUSH_DELAY = 180 # In seconds
+FORCE_FLUSH_INTERVAL = 300 # In seconds
+
+
+class kafkaConsumerToS3:
+    def __init__(self):
+        # Confluent consumer
+        self.consumer = Consumer(CONSUMER_CONF)
+
+        # AWS S3
+        self.s3 = boto3.client("s3", region_name=AWS_REGION)
+
+        # Windows
+        self.windows = defaultdict(lambda: defaultdict(list)) # self.windows[floored_ts][slug] = [msg, msg, ...]
+        self.running = True
+
+        # Periodic flush
+        self.lock = threading.Lock()
+
+        self.flush_thread = threading.Thread(target=self._periodic_flush, daemon=True) # Runs in the background
+        
+
+        # Flush on shutdown
+        signal.signal(signal.SIGINT, self._shutdown)
+        signal.signal(signal.SIGTERM, self._shutdown)
+
+    def _get_window_start(self, ts:datetime) -> datetime:
+        floored_ts = ts.minute - (ts.minute % WINDOW_MINUTES)
+        return ts.replace(minute=floored_ts, second=0, microsecond=0)
     
-    def safeDeserialization(self, message):
+    def _upload_to_s3(self, messages, window_start, symbol):
+        if not messages:
+            return
+        first_index_ts = datetime.fromisoformat(messages[0]["datetimeIso"]) #.strftime("%H:%M%S%f")[:-3]
+        last_index_ts = datetime.fromisoformat(messages[-1]["datetimeIso"]) #.strftime("%H:%M%S%f")[:-3]
+        diff = first_index_ts - last_index_ts
+
+        if int(diff.total_seconds()) <= 900:
+            pass
+        else:
+            logger.warning("Crypto price timestamp difference between batch messages is too wide (>900sec).",
+                           extra={"Index [0]":str(messages[0]), "Index [-1]":str(messages[-1])})
+            pass
+
+        year = first_index_ts.year
+        month = first_index_ts.month
+        day = first_index_ts.day
+        hour = first_index_ts.hour
+        minute = first_index_ts.minute
+        unq = secrets.token_hex(4)
+        messages_len = len(messages)
+        last_hour = last_index_ts.hour
+        last_minute = last_index_ts.minute
+
+        filename = f"len={messages_len}_first={hour}:{minute}_last={last_hour}:{last_minute}_unq={unq}"
+        key = f"{symbol}/year={year}/month={month}/day={day}/{filename}.jsonl"
+        
+        # now = datetime.now(timezone.utc).strftime('%H:%M:%S%f')[:-3]
+        # key = f"{symbol}/{window_start.strftime('%Y-%m-%d_%H:%M')}/{now}.jsonl"
+        buffer = io.BytesIO()
+
+        for msg in messages:
+            buffer.write(json.dumps(msg).encode("utf-8"))
+            buffer.write(b"\n")
+        buffer.seek(0)
         try:
-            return json.loads(message.decode('utf-8')) if message else None
-        except (UnicodeDecodeError, json.JSONDecodeError) as e:
-            logger.error(f"Skipping bad message (raw byte: {message!r}).")
-            return None 
-    
-    def _create_consumer(self):
-        return KafkaConsumer(
-            self.topic_name,
-            bootstrap_servers=[f"{self.host}:{self.port}"],
-            value_deserializer=self.safeDeserialization, 
-            auto_offset_reset='latest',
-            group_id="my-group-1",
-            enable_auto_commit=False
-        )
-    def isTimetoFlush(self):
-        if not hasattr(self, "last_flush_time"):
-            self.last_flush_time = time.monotonic()
-            return False
-        now = time.monotonic()
-        return (now - self.last_flush_time) >= 300
+            self.s3.upload_fileobj(buffer, BUCKET, key)
+            logger.info(
+                f"Uploaded {len(messages)} {symbol} msgs with timestamp {window_start} "
+                f"to s3://{BUCKET}/{key}"
+            )
+        except (BotoCoreError, ClientError) as e:
+            logger.error(f"Failed upload {symbol} timestamp {window_start}: {e}", exc_info=True)
+        finally:
+            buffer.close()
 
-    
-    def toBucket(self, message: dict):
-        # Only flush existing batch
-        if len(self.batch) >= self.batch_size or self.isTimetoFlush():
-            if self.batch:  # flush only if not empty
-                df = pd.DataFrame(self.batch)
-                buffer = io.BytesIO()
-                df.to_parquet(buffer, engine="pyarrow", index=False)
-                buffer.seek(0)
+    def _flush_expire_windows(self, force=False):
+        """
+        Iterate every window_start, if now > window_end then flush that window.
+        After flushing delete flushed window from dict.
+        """
+        now = datetime.now(timezone.utc)
+        expired = []
+        
+        with self.lock:
+            for window_start, symbol_batches in self.windows.items():
+                window_end = window_start + timedelta(minutes=WINDOW_MINUTES)
+                if force or now > window_end + timedelta(seconds=FLUSH_DELAY):
+                    for symbol, msgs in symbol_batches.items():
+                        self._upload_to_s3(msgs, window_start, symbol)
+                    expired.append(window_start)
 
-                utc_now = datetime.now(timezone.utc).strftime("%Y%m%d-%H%M%S%f")[:-3]
-                key = f"batch_len={len(self.batch)}_{utc_now}.parquet"
+            for win in expired:
+                del self.windows[win]
+        logger.info("Expired windows flushed.")
 
-                self.s3_uploader.upload_batch(buffer, key=key)
-                logger.info(f"Flushed {len(self.batch)} messages to {key}")
+    def _periodic_flush(self):
 
-                self.batch = []
-                self.last_flush_time = time.monotonic()
+        while self.running:
+            time.sleep(FORCE_FLUSH_INTERVAL)
+            logger.info("Periodic flush triggered.")
 
-        # Always append *after* flushing
-        self.batch.append(message)
-        if len(self.batch) >= self.batch_size:
-            df = pd.DataFrame(self.batch)
-            buffer = io.BytesIO()
-            df.to_parquet(buffer, engine="pyarrow", index=False)
-            buffer.seek(0)
+            self._flush_expire_windows(force=True)
+        
+    def _shutdown(self, signum, frame):
+        logger.info("Shutdown received. Flushing all windows.")
+        self.running = False
+        self._flush_expire_windows()
+        self.consumer.close()
+        self.running = False
 
-            utc_now = datetime.now(timezone.utc).strftime("%Y%m%d-%H%M%S%f")[:-3]
-            key = f"batch_len={len(self.batch)}_{utc_now}.parquet"
+    def run(self):
+        # Check if topic has been created
+        # Topic created by producer
+        while True:
+            metadata = self.consumer.list_topics(timeout=10)
+            if TOPIC in metadata.topics:
+                logger.info(f"Topic: '{TOPIC}' found. Subscribing...")
+                logger.info(f"Starting periodic flush...",
+                            extra={"FORCE_FLUSH_INTERVAL":FORCE_FLUSH_INTERVAL})
+                self.consumer.subscribe([TOPIC])
+                self.flush_thread.start() # Start periodic flush
+                break
+            else:
+                logger.info(f"Topic: '{TOPIC}' not found yet. Waiting for producer to create it.")
+                time.sleep(5)
 
-            self.s3_uploader.upload_batch(buffer, key=key)
-            logger.info(f"Flushed {len(self.batch)} messages to {key}")
+        # Poll message
+        try:
+            while self.running:
+                try:
+                    msg = self.consumer.poll(1.0)
+                    if msg is None:
+                        continue
+                    if msg.error():
+                        if msg.error().code() == KafkaError._PARTITION_EOF:
+                            continue
+                        else:
+                            raise KafkaException(msg.error()) # Raised exception will be catched by the "except:" block.
 
-            self.batch = []
-            self.last_flush_time = time.monotonic()
+                    try:
+                        record = json.loads(msg.value().decode('utf-8'))
+                    except Exception as e:
+                        logger.error("Failed to deserialize message.", exc_info=e)
 
-        logger.info(f"Batch status: {len(self.batch)}/{self.batch_size}")
+                    ts = datetime.fromisoformat(record["datetimeIso"])
+                    ts_floored = self._get_window_start(ts)
+                    symbol = record["slug"]
 
+                    self.windows[ts_floored][symbol].append(record)
 
-def main():
-    # Create consumer object
-    consumer = resilientConsumer(host=os.environ['HOST'],
-                                 port=os.environ['PORT'],
-                                 topic_name=os.environ['TOPIC_NAME'],
-                                 bucket_name=os.environ['BUCKET_NAME'])
-    # Send data to S3 bucket
-    for message in consumer:
-        logger.info(f"message received: {message.value}")
-        consumer.toBucket(message=message.value)
+                    self._flush_expire_windows()
+
+                except Exception as e:
+                    logger.error("Error while consuming messages", exc_info=e)
+
+        except Exception as e:
+            logger.error("Fatal error in consumer loop.", exc_info=e)
+        finally:
+            logger.info("Closing consumer gracefully...")
+            self._flush_expire_windows(force=True)
+            self.consumer.close()
 
 if __name__ == "__main__":
-    main()
-
-
-
+    consumer = kafkaConsumerToS3()
+    consumer.run()
